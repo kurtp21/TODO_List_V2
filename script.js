c1fd1a64a80ad0e581ee735095c85fbfe35a232e
kurtpagal <zja641@usask.ca>
2023-06-06 22:19:06 -0600

    made a new list button appear after adding task

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const container = document.getElementById("container"); 
 const inputBox = document.getElementById("input-box");
 const tasks = document.getElementById("task");
-const newList = document.getElementById("new-list"); 
+const freshButton = document.getElementById("new-button"); 
 
 function addTask() {
     let li = document.createElement("li");
-    let button = document.createElement("button");
-    let buttonDisplay = button.style.display; 
+    let buttonDisplay = freshButton.style.display; 
 
     if (inputBox.value == '') {
         alert("Cannont add an empty task!");
     } else {
         li.innerHTML = inputBox.value; 
+        freshButton.style.display = 'block';
         tasks.appendChild(li);
     }
-}
-
-function showButton() {
-    
 }
